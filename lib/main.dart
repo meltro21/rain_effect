f49f0ba01ae1f8449f6af602e80b684bf712d584
meltro <meltro212121@gmail.com>
2022-07-29 14:34:05 +0500

    learned the basics 1- sync different animaiton

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:dotted_decoration/dotted_decoration.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:indexed/indexed.dart';
 import 'package:rain_effect/controllers/rain_controller.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -32,51 +34,49 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
-  late final AnimationController _controller = AnimationController(
-    duration: const Duration(seconds: 2),
-    vsync: this,
-  )..repeat(reverse: false);
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> slide;
+  late Animation<double> animateOpacity;
+  late Animation<double> animateScale;
 
-  late final AnimationController _controller1 = AnimationController(
-    duration: const Duration(seconds: 1),
-    vsync: this,
-  )..repeat(reverse: false);
+  var show = false;
 
-  late final Animation<Offset> _offsetAnimation1 = Tween<Offset>(
-    begin: Offset(0.1, 0.0),
-    end: const Offset(0.1, 3),
-  ).animate(CurvedAnimation(
-    parent: _controller,
-    curve: Curves.linear,
-  ));
-
-  late var sizeAnimation =
-      Tween<double>(begin: 100.0, end: 200.0).animate(_controller);
-  late var colorAnimation =
-      ColorTween(begin: Colors.blue, end: Colors.yellow).animate(_controller);
-  late var opacityAnimation =
-      Tween<double>(begin: 1, end: 0.0).animate(_controller);
-
-  // late final Animation<Offset> _offsetAnimation2 = Tween<Offset>(
-  //   begin: Offset(70.5, 0.0),
-  //   end: const Offset(70.5, 9),
-  // ).animate(CurvedAnimation(
-  //   parent: _controller,
-  //   curve: Curves.fastOutSlowIn,
-  // ));
-
-  late Animation<double> animation = Tween<double>(begin: 0, end: 15).animate(
-      CurvedAnimation(parent: _controller, curve: Curves.fastOutSlowIn));
-
-  RainController rainController = Get.put(RainController());
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    _controller.addListener(() {
-      setState(() {});
+    controller =
+        AnimationController(duration: const Duration(seconds: 1), vsync: this);
+    slide = Tween<double>(begin: 0, end: 300).animate(CurvedAnimation(
+        parent: controller,
+        curve: Interval(
+          0.1,
+          0.6,
+        )));
+    animateOpacity = Tween<double>(begin: 1, end: 0)
+        .animate(CurvedAnimation(parent: controller, curve: Interval(0.6, 1)));
+    animateScale = Tween<double>(begin: 0, end: 1)
+        .animate(CurvedAnimation(parent: controller, curve: Interval(0.6, 1)));
+    controller.addListener(() {
+      setState(() {
+        if (controller.value > 0.6) {
+          show = true;
+        }
+        controller.forward();
+      });
     });
+    controller.addStatusListener((status) {
+      print('value of controller is ${controller.value}');
+      if (status == AnimationStatus.completed) {
+        controller.repeat();
+        setState(() {
+          show = false;
+        });
+      }
+    });
+    controller.forward();
   }
 
   @override
@@ -84,205 +84,64 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
     var mediaHeight = MediaQuery.of(context).size.height;
     var mediaWidth = MediaQuery.of(context).size.width;
 
-    print('duration is ${_controller.duration}');
+    //print('duration is ${_controller.duration}');
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          height: mediaHeight,
-          width: mediaWidth,
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  Color(
-                    0xff202020,
-                  ),
-                  Color(0xff111119)
-                ]),
-          ),
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            Opacity(
-              opacity: opacityAnimation.value,
-              child: Container(
-                margin: EdgeInsets.only(top: sizeAnimation.value),
-                height: 20,
-                width: 3,
-                color: colorAnimation.value,
-              ),
-            ),
-          ]),
-
-          //Stack with colum inside with traisiition
-          // Stack(
-          //   children: [
-          //     SlideTransition(
-          //       position: _offsetAnimation1,
-          //       child: Container(
-          //         height: 60,
-          //         width: 30,
-          //         //color: Colors.red,
-          //         child: Column(
-          //             crossAxisAlignment: CrossAxisAlignment.start,
-          //             children: [
-          //               Container(
-          //                 decoration: BoxDecoration(
-          //                     gradient: LinearGradient(
-          //                         begin: Alignment.topCenter,
-          //                         end: Alignment.bottomCenter,
-          //                         colors: [
-          //                       Color.fromRGBO(255, 255, 255, 0),
-          //                       Color.fromRGBO(255, 255, 255, 0.5),
-          //                     ])),
-          //                 height: 20,
-          //                 width: 3,
-          //               ),
-          //               SizeTransition(
-          //                 sizeFactor: _controller,
-          //                 child: Container(
-          //                   color: Colors.pink,
-          //                   height: 2,
-          //                   width: 3,
-          //                 ),
-          //               ),
-          //             ]),
-          //       ),
-          //     )
-          //   ],
-          // )
-          //
-          //
-          // Indexer(
-          //   children: [
-          //     Indexed(
-          //       index: 2,
-          //       child: Positioned(
-          //         child: Container(
-          //           // height: 100,
-          //           // width: 20,
-          //           //color: Colors.red,
-          //           child: SlideTransition(
-          //             position: _offsetAnimation1,
-          //             child: Column(children: [
-          //               Container(
-          //                 decoration: BoxDecoration(
-          //                     gradient: LinearGradient(
-          //                         begin: Alignment.topCenter,
-          //                         end: Alignment.bottomCenter,
-          //                         colors: [
-          //                       Color.fromRGBO(255, 255, 255, 0),
-          //                       Color.fromRGBO(255, 255, 255, 0.5),
-          //                     ])),
-          //                 width: 1,
-          //                 height: 30,
-          //               ),
-          //               // Container(
-          //               //   height: 10,
-          //               //   width: 15,
-          //               //   decoration: BoxDecoration(
-          //               //       color: Color.fromRGBO(255, 255, 255, 0.5),
-          //               //       border: Border(top: BorderSide(width: 1))),
-          //               // ),
-          //             ]),
-          //           ),
-          //         ),
-          //       ),
-          //     ),
-          //     // Indexed(
-          //     //   index: 1,
-          //     //   child: AnimatedContainer(
-          //     //       duration: Duration(seconds: 5),
-          //     //       width: 35,
-          //     //       height: 50,
-          //     //       decoration: BoxDecoration(
-          //     //         color: Colors.white,
-          //     //         border: Border(
-          //     //             top: BorderSide(width: 1, style: BorderStyle.solid)),
-          //     //       )),
-          //     // ),
-          //     // Indexed(
-          //     //     index: 1,
-          //     //     child: SizeTransition(
-          //     //       sizeFactor: CurvedAnimation(
-          //     //         curve: Curves.fastOutSlowIn,
-          //     //         parent: _controller,
-          //     //       ),
-          //     //       child: Container(
-          //     //         height: 10,
-          //     //         width: 15,
-          //     //         decoration: BoxDecoration(
-          //     //             color: Color.fromRGBO(255, 255, 255, 0.5),
-          //     //             border: Border(top: BorderSide(width: 1))),
-          //     //       ),
-          //     //     )),
-          //     // Indexed(
-          //     //   index: 1,
-          //     //   child: Opacity(
-          //     //     opacity: 0.5,
-          //     //     child: Positioned(
-          //     //       child: SlideTransition(
-          //     //         position: _offsetAnimation2,
-          //     //         child: Container(
-          //     //           decoration: BoxDecoration(
-          //     //               gradient: LinearGradient(
-          //     //                   begin: Alignment.topCenter,
-          //     //                   end: Alignment.bottomCenter,
-          //     //                   colors: [
-          //     //                 Color.fromRGBO(255, 255, 255, 0),
-          //     //                 Color.fromRGBO(255, 255, 255, 0.5),
-          //     //               ])),
-          //     //           width: 1,
-          //     //           height: 30,
-          //     //         ),
-          //     //       ),
-          //     //     ),
-          //     //   ),
-          //     // ),
-          //   ],
-          // ),
-
-          // // Stack(
-          // //   children: [
-          // //     //Rain Drop
-          // //     SlideTransition(
-          // //       position: _offsetAnimation,
-          // //       child: Container(
-          // //         decoration: BoxDecoration(
-          // //             gradient: LinearGradient(
-          // //                 begin: Alignment.topCenter,
-          // //                 end: Alignment.bottomCenter,
-          // //                 colors: [
-          // //               Color.fromRGBO(255, 255, 255, 0),
-          // //               Color.fromRGBO(255, 255, 255, 0.5),
-          // //             ])),
-          // //         width: 1,
-          // //         height: 30,
-          // //       ),
-          // //     ),
-          // //     //back rain drop
-          // //     // SlideTransition(
-          // //     //   position: _offsetAnimation,
-          // //     //   child: Card(
-          // //     //     elevation: 10,
-          // //     //     child: Container(
-          // //     //       decoration: BoxDecoration(
-          // //     //           gradient: LinearGradient(
-          // //     //               begin: Alignment.topCenter,
-          // //     //               end: Alignment.bottomCenter,
-          // //     //               colors: [
-          // //     //             Color.fromRGBO(255, 255, 255, 0),
-          // //     //             Color.fromRGBO(255, 255, 255, 0.5),
-          // //     //           ])),
-          // //     //       width: 1,
-          // //     //       height: 30,
-          // //     //     ),
-          // //     //   ),
-          // //     // ),
-          // //   ],
-          // // ),
+          child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Color(0xff202020), Color(0xff111119)]),
         ),
-      ),
+        height: mediaHeight,
+        width: mediaWidth,
+        child: Stack(children: [
+          show == false
+              ? Positioned(
+                  top: slide.value,
+                  child: Container(
+                    height: 40,
+                    width: 2.5,
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Color.fromRGBO(255, 255, 255, 0),
+                        Color.fromRGBO(255, 255, 255, 0.25)
+                      ],
+                    )),
+                    child: Container(),
+                  ),
+                )
+              : SizedBox(),
+          show == true
+              ? Positioned(
+                  top: 330,
+                  child: Transform.scale(
+                    scale: animateScale.value,
+                    child: Opacity(
+                      opacity: animateOpacity.value,
+                      child: Container(
+                        // decoration: BoxDecoration(
+                        //     border: Border(top: BorderSide(width: 2)),
+                        //     color: Color.fromRGBO(255, 255, 255, 0.5),
+                        //     borderRadius: BorderRadius.circular(50)),
+                        decoration: DottedDecoration(
+                          shape: Shape.box,
+                          borderRadius: BorderRadius.circular(
+                              50), //remove this to get plane rectange
+                        ),
+                        height: 10,
+                        width: 7,
+                      ),
+                    ),
+                  ),
+                )
+              : SizedBox()
+        ]),
+      )),
     );
   }
 }
