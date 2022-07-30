0c011e5c3c5915cfd0a707b7735716e39d0a6ab4
meltro <meltro212121@gmail.com>
2022-07-30 12:52:38 +0500

    rain effect 	rain drop along with splat appear at certain intervals

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:delayed_display/delayed_display.dart';
 import 'package:dotted_decoration/dotted_decoration.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -37,9 +38,16 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage>
     with SingleTickerProviderStateMixin {
   late AnimationController controller;
-  late Animation<double> slide;
-  late Animation<double> animateOpacity;
-  late Animation<double> animateScale;
+
+  late List<Animation<double>> slideAnimateList = [];
+  late List<Animation<double>> animateDropOpacityList = [];
+  late List<Animation<double>> hideDropList = [];
+  late List<Animation<double>> animateScaleList = [];
+  var count = 10.0;
+  List<double> pos = [];
+  List<double> slideRandomList = [];
+  var uniformDistribution = 0;
+  var rng;
 
   var show = false;
 
@@ -48,32 +56,59 @@ class _MyHomePageState extends State<MyHomePage>
     // TODO: implement initState
     super.initState();
     controller =
-        AnimationController(duration: const Duration(seconds: 1), vsync: this);
-    slide = Tween<double>(begin: 0, end: 300).animate(CurvedAnimation(
-        parent: controller,
-        curve: Interval(
-          0.1,
-          0.6,
-        )));
-    animateOpacity = Tween<double>(begin: 1, end: 0)
-        .animate(CurvedAnimation(parent: controller, curve: Interval(0.6, 1)));
-    animateScale = Tween<double>(begin: 0, end: 1)
-        .animate(CurvedAnimation(parent: controller, curve: Interval(0.6, 1)));
+        AnimationController(duration: const Duration(seconds: 10), vsync: this);
+
+    for (var i = 0; i < 100; i++) {
+      rng = Random();
+      // if (i % 10 == 0) {
+      //   uniformDistribution += 2;
+      // }
+      var num = rng.nextInt(7) + uniformDistribution;
+      var startDrop = num / 10;
+      var endDrop = startDrop + 0.06;
+      var startSplat = endDrop;
+      var endSplat = endDrop + 0.04;
+      int slideRandom = rng.nextInt(50);
+      var a = slideRandom.toDouble();
+      pos.add(count);
+      slideRandomList.add(a);
+
+      var slideAnimate =
+          Tween<double>(begin: 0, end: 290 + a).animate(CurvedAnimation(
+              parent: controller,
+              curve: Interval(
+                startDrop,
+                endDrop,
+              )));
+      slideAnimateList.add(slideAnimate);
+      var animateDropOpacity = Tween<double>(begin: 1, end: 0).animate(
+          CurvedAnimation(
+              parent: controller, curve: Interval(startSplat, endSplat)));
+      animateDropOpacityList.add(animateDropOpacity);
+      var hideDrop = Tween<double>(begin: 1, end: 0).animate(CurvedAnimation(
+          parent: controller, curve: Interval(endDrop, endDrop)));
+      hideDropList.add(hideDrop);
+      var animateScale = Tween<double>(begin: 0, end: 1).animate(
+          CurvedAnimation(
+              parent: controller, curve: Interval(startSplat, endSplat)));
+      animateScaleList.add(animateScale);
+      count += 10;
+    }
     controller.addListener(() {
       setState(() {
-        if (controller.value > 0.6) {
-          show = true;
-        }
-        controller.forward();
+        // if (controller.value > 0.6) {
+        // }
+        // controller.forward();
       });
     });
     controller.addStatusListener((status) {
       print('value of controller is ${controller.value}');
       if (status == AnimationStatus.completed) {
+        //Future.delayed(Duration(microseconds: 200));
         controller.repeat();
-        setState(() {
-          show = false;
-        });
+        // setState(() {
+        //   show = false;
+        // });
       }
     });
     controller.forward();
@@ -97,49 +132,48 @@ class _MyHomePageState extends State<MyHomePage>
         height: mediaHeight,
         width: mediaWidth,
         child: Stack(children: [
-          show == false
-              ? Positioned(
-                  top: slide.value,
+          for (int i = 0; i < 100; i++)
+            Positioned(
+              left: pos[i],
+              top: slideAnimateList[i].value,
+              child: Opacity(
+                opacity: hideDropList[i].value,
+                child: Container(
+                  height: 30,
+                  width: 2,
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Color.fromRGBO(255, 255, 255, 0),
+                      Color.fromRGBO(255, 255, 255, 0.25)
+                    ],
+                  )),
+                  child: Container(),
+                ),
+              ),
+            ),
+          for (int i = 0; i < 100; i++)
+            Positioned(
+              left: pos[i],
+              top: 330 + slideRandomList[i],
+              child: Transform.scale(
+                scale: animateScaleList[i].value,
+                child: Opacity(
+                  opacity: animateDropOpacityList[i].value,
                   child: Container(
-                    height: 40,
-                    width: 2.5,
-                    decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [
-                        Color.fromRGBO(255, 255, 255, 0),
-                        Color.fromRGBO(255, 255, 255, 0.25)
-                      ],
-                    )),
-                    child: Container(),
-                  ),
-                )
-              : SizedBox(),
-          show == true
-              ? Positioned(
-                  top: 330,
-                  child: Transform.scale(
-                    scale: animateScale.value,
-                    child: Opacity(
-                      opacity: animateOpacity.value,
-                      child: Container(
-                        // decoration: BoxDecoration(
-                        //     border: Border(top: BorderSide(width: 2)),
-                        //     color: Color.fromRGBO(255, 255, 255, 0.5),
-                        //     borderRadius: BorderRadius.circular(50)),
-                        decoration: DottedDecoration(
-                          shape: Shape.box,
-                          borderRadius: BorderRadius.circular(
-                              50), //remove this to get plane rectange
-                        ),
-                        height: 10,
-                        width: 7,
-                      ),
+                    decoration: DottedDecoration(
+                      shape: Shape.box,
+                      borderRadius: BorderRadius.circular(
+                          50), //remove this to get plane rectange
                     ),
+                    height: 15,
+                    width: 7,
                   ),
-                )
-              : SizedBox()
+                ),
+              ),
+            )
         ]),
       )),
     );
